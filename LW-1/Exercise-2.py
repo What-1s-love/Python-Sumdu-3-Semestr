5f892c942c1f9b5b2ebe2f5d02a164f66d86f18e
NatanNed <natan.nedaikhlib@gmail.com>
2024-08-29 13:48:52 +0200

    Fix for loop syntax and table formatting.

--- LW-1/Exercise-2.py
+++ LW-1/Exercise-2.py
@@ -2,10 +2,10 @@
 numbers = [1,3,5,9,17]
 
 # Print the header of the table 
-print(f"{'Number': < 10} {'Square': < 10} {'Cube': < 10}")
+print(f"{'Number': <10} {'Square': <10} {'Cube': <10}")
 
 # Calculate squares and cubes, and output the results for num in numbers:
 for num in numbers:
     square = num ** 2
     cube = num ** 3
-    print(f"{num: < 10} {square: < 10} {cube: < 10}")
+    print(f"{num: <10} {square: <10} {cube: <10}")
