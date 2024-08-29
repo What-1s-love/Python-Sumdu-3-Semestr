83d4750262624a8cfc5ffa8f928735f67202cb3c
NatanNed <natan.nedaikhlib@gmail.com>
2024-08-29 12:56:46 +0200

    Add Exercise-1

---
+++ LW-1/Exercise-1.py
@@ -0,0 +1,21 @@
+a = int(input("Enter the value of a (from 1 to 100): "))
+b = int(input("Enter the value of b (from 1 to 100): "))
+
+# Checking if the entered values are correct
+if not (1 <= a <= 100) or not (1 <= b <= 100):
+    print("Values of a and b must be in the range from 1 to 100.")
+else:
+    # Calculating the value of X
+    if a < b:
+        X = (a * 3 - 5) / b
+        condition = "a < b"
+    elif a == b:
+        X = -4
+        condition = "a = b"
+    else:
+        X = (a**4 + b) / a
+        condition = "a > b"
+
+# Outputting the result
+print("The value of X: ", X) 
+print("Condition used:", condition)
