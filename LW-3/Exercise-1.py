c2777ca9eed5ca6f42051dfe11b1ea2ca2f76153
NatanNed <natan.nedaikhlib@gmail.com>
2024-09-04 18:57:06 +0200

    Add program to extract every third letter from a user input string.

---
+++ LW-3/Exercise-1.py
@@ -0,0 +1,8 @@
+# Assign a string to the variable
+my_string = input("Enter your string: ")
+
+# Get every third letter 
+third_letter_slice = my_string[::3]
+
+# Print the result
+print("Every third letter:", third_letter_slice)
